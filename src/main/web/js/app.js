f573e68776f2a4ae0d91f2a2129887247057979f
wbelka94 <wbelka94@gmail.com>
2018-06-14 14:49:16 +0200

    POST, PUT, DELETE course

--- src/main/web/js/app.js
+++ src/main/web/js/app.js
@@ -12,10 +12,10 @@ class Student {
     }
 
     addSubscribe(){
-        this.firstname.subscribe(this.update.bind(this));
-        this.lastname.subscribe(this.update.bind(this));
-        this.birthday.subscribe(this.update.bind(this));
-        this.index.subscribe(this.update.bind(this));
+        this.firstname.subscribe(this.PUT.bind(this));
+        this.lastname.subscribe(this.PUT.bind(this));
+        this.birthday.subscribe(this.PUT.bind(this));
+        this.index.subscribe(this.PUT.bind(this));
     }
 
     getData(){
@@ -26,7 +26,7 @@ class Student {
         });
     }
 
-    update(){
+    PUT(){
         console.log(this.getData());
         $.ajax({
             url: "http://localhost:8888/myapp/students/" + ko.toJS(this.index),
@@ -65,12 +65,72 @@ class Student {
         });
     }
 }
+class Course {
+    constructor(data = {name: "", lecturer: "", uid: ""}){
+        this.name = new ko.observable(data.name);
+        this.lecturer = new ko.observable(data.lecturer);
+        this.uid = new ko.observable(data.uid);
+        this.addSubscribe();
+    }
+
+    addSubscribe(){
+        this.name.subscribe(this.PUT.bind(this));
+        this.lecturer.subscribe(this.PUT.bind(this));
+        this.uid.subscribe(this.PUT.bind(this));;
+    }
+
+    getData(){
+        return ko.toJSON({
+            name: this.name,
+            lecturer: this.lecturer
+        });
+    }
+
+    PUT(){
+        $.ajax({
+            url: "http://localhost:8888/myapp/courses/" + ko.toJS(this.uid),
+            method: "PUT",
+            data: this.getData(),
+            async: false,
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            }
+        });
+    }
+
+    DELETE(){
+        $.ajax({
+            url: "http://localhost:8888/myapp/courses/" + ko.toJS(this.uid),
+            method: "DELETE",
+            async: false,
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            }
+        });
+    }
+
+    POST(){
+        $.ajax({
+            url: "http://localhost:8888/myapp/courses/",
+            method: "POST",
+            async: false,
+            data: this.getData(),
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            }
+        });
+    }
+}
 var gradesSystemModel = function(){
     var self = this;
 
     this.students = ko.observableArray([]);
     this.studentToAdd = new Student();
     this.courses = ko.observableArray([]);
+    this.courseToAdd = new Course();
     this.grades = ko.observableArray([]);
 
 
@@ -89,30 +149,44 @@ var gradesSystemModel = function(){
         }).responseText);
         ko.mapping.fromJS(studnets,mapping,self.students);
     }
-
     this.getStudents();
-
-
     this.addStudent = function(){
         self.studentToAdd.POST();
         self.getStudents();
         self.studentToAdd = new Student();
     }
-
     this.deleteStudent = function(student){
         student.DELETE();
         self.getStudents();
     }
 
-    function getCourses() {
-        return JSON.parse($.ajax({
+    this.getCourses = function() {
+        var mapping = {
+            create: function(options) {
+                return new Course(options.data);
+            }
+        };
+        var courses = JSON.parse($.ajax({
             url: apiSerwer + 'courses',
             async: false,
             headers: {
-                Accept: "application/json"
+                Accept: "application/json",
             }
         }).responseText);
+        ko.mapping.fromJS(courses,mapping,self.courses);
     }
+    this.getCourses();
+    this.addCourse= function(){
+        self.courseToAdd.POST();
+        self.getCourses();
+        self.courseToAdd = new Course();
+    }
+    this.deleteCourse = function(course){
+        course.DELETE();
+        self.getCourses();
+    }
+
+
     function getGradesForStudent(index){
         return JSON.parse($.ajax({
             url: apiSerwer + 'students/'+index+'/grades',
